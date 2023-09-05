a55da4a988eefc69daef015fb28759689fcba59f
Dmitry Dolmatov <dima_dolmatov@mail.ru>
2023-09-05 13:37:09 +0700

    hometest2.1

--- OnlineShopUI.java
+++ OnlineShopUI.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class OnlineShopUI {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите ID товара: ");
-        int productId = scanner.nextInt();
-        System.out.print("Введите количество: ");
-        int quantity = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите ID товара: ");
+            int productId = scanner.nextInt();
+            System.out.print("Введите количество: ");
+            int quantity = scanner.nextInt();
 
-        try {
-            double totalPrice = ShopManager.purchaseProduct(productId, quantity);
-            System.out.println("Общая стоимость: $" + totalPrice);
-        } catch (ProductNotFoundException e) {
-            System.out.println(e.getMessage());
+            try {
+                double totalPrice = ShopManager.purchaseProduct(productId, quantity);
+                System.out.println("Общая стоимость: $" + totalPrice);
+            } catch (ProductNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
